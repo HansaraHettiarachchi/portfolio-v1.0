d63060cc3f9b3c6bfaede0af57029189093ed70e
Hansara <hansarasasanka@gmail.com>
2025-11-03 18:50:40 +0530

    certificate update

--- src/components/home/Timeline.tsx
+++ src/components/home/Timeline.tsx
@@ -1,3 +1,8 @@
+import oci_foundations_associate from "../../assets/images/certificates/oci_ai_fundation_associate.jpg";
+import oci_gen_ai_professional from "../../assets/images/certificates/oci_gen_ai_professional.jpg";
+import oci_gen_ai_professional_badge from "../../assets/images/certificates/oci_ai_fundations_associate_badge.png";
+import oci_foundations_associate_badge from "../../assets/images/certificates/oci_ai_fundations_associate_badge.png";
+import { GiTiredEye } from "react-icons/gi";
 
 type TimelineData = {
     side: string;
@@ -7,10 +12,7 @@ type TimelineData = {
     skills: string[];
     date: string;
     type: string;
-    images?: {
-        img: string;
-        link: string;
-    }[];
+    images?: { img: string; link: string; }[];
 }
 
 const timelineData: TimelineData[] = [
@@ -23,8 +25,8 @@ const timelineData: TimelineData[] = [
             "Learned to work with Microsoft SQL Server",
             "Developed a small CRUD application using C#.NET and SQL Server",
         ],
-        type: "Skills :",
-        skills: ["C#.net", "MS SQL"],
+        type: "Skills:",
+        skills: ["C#.NET", "MS SQL"],
         date: "April 2022 - Nov 2022"
     },
     {
@@ -35,10 +37,10 @@ const timelineData: TimelineData[] = [
             "Completed Diploma awarded by UK Awards",
             "Completed Higher Diploma awarded by UK Awards",
             "Commenced BSc studies at Birmingham City University",
-            "Developed several real word projects with gathered skills",
+            "Developed several real-world projects with gathered skills",
         ],
-        type: "Skills :",
-        skills: ["Java", "php", "HTML", "CSS", "JavaScript", "JQuery", "Bootstrap", "MySQL", "OOP", "JSP", "React Native", "OOPC", "OODP"],
+        type: "Skills:",
+        skills: ["Java", "PHP", "HTML", "CSS", "JavaScript", "jQuery", "Bootstrap", "MySQL", "OOP", "JSP", "React Native", "OOPC", "OODP"],
         date: "May 2023 - Present"
     },
     {
@@ -52,9 +54,9 @@ const timelineData: TimelineData[] = [
             "Optimized database structure for scalability and fast data retrieval.",
             "Provided training and documentation for smooth system adoption."
         ],
-        type: "Techstack :",
+        type: "Tech Stack:",
         skills: ["HTML", "CSS", "JavaScript", "PHP", "Bootstrap", "jQuery"],
-        date: "April 2024 – November Jul"
+        date: "April 2024 – July 2024"
     },
     {
         side: 'left',
@@ -67,7 +69,7 @@ const timelineData: TimelineData[] = [
             "Explored monolithic and layered architecture patterns in backend development.",
             "Developed a full-stack CRUD application applying learned concepts across frontend and backend."
         ],
-        type: "Skills :",
+        type: "Skills:",
         skills: ["Spring Boot", "React", "TypeScript", "Postman", "Monolithic Architecture", "Layered Architecture"],
         date: "Aug 2022 – Oct 2024"
     },
@@ -83,7 +85,7 @@ const timelineData: TimelineData[] = [
             "Implemented core institute management features including user roles, class scheduling, salary tracking, and student-teacher records.",
             "Focused on modular architecture for scalability and maintainability, applying layered and monolithic design principles."
         ],
-        type: "Learned Skills and Tech Stack :",
+        type: "Skills:",
         skills: ["Python", "TensorFlow", "Keras", "Java", "OpenCV", "DeepFace"],
         date: "Jul 2024 – Jan 2025"
     },
@@ -96,7 +98,7 @@ const timelineData: TimelineData[] = [
             "Utilized JDBC Template to connect with a large-scale Microsoft SQL Server database.",
             "Developed both paginated and non-paginated RESTful endpoints following layered architecture principles."
         ],
-        type: "Learned Skills and Tech Stack :",
+        type: "Skills:",
         skills: ["JDBC Template", "Spring Boot", "C#.NET", "REST"],
         date: "March 2025 – April 2025"
     },
@@ -110,7 +112,7 @@ const timelineData: TimelineData[] = [
             "Collaborated with senior developers to understand Laravel architecture and MVC patterns.",
             "Gained hands-on experience in deploying and testing web applications in a production environment."
         ],
-        type: "Tech Stack :",
+        type: "Skills:",
         skills: ["HTML", "CSS", "JavaScript", "PHP", "Laravel"],
         date: "May 2025 – Jul 2025"
     },
@@ -125,27 +127,27 @@ const timelineData: TimelineData[] = [
             "Redesigned several UI components to improve usability and consistency.",
             "Developed the backend using a layered architecture with Node.js, Express, Prisma, and MySQL."
         ],
-        type: "Learned Skills and Tech Stack :",
+        type: "Skills:",
         skills: ["React", "TypeScript", "Node.js", "Express.js", "Prisma", "MySQL"],
         date: "01 Aug 2024 – 25 Aug 2025"
     },
     {
         side: 'right',
         title: 'Saddha Web Front-End',
-        institute: 'By Myself',
+        institute: 'Self-Studied',
         content: [
             "Designed the front-end based on a Figma prototype.",
             "Self-studied Next.js and Tailwind CSS to build responsive UI components.",
             "Developed and styled the full layout using modern frontend practices."
         ],
-        type: "Learned Skills and Tech Stack :",
-        skills: ["Next.js", "Tailwind"],
+        type: "Skills:",
+        skills: ["Next.js", "Tailwind CSS"],
         date: "27 Aug 2025 – 29 Aug 2025"
     },
     {
         side: 'right',
         title: 'Spring Boot Backend – Adora E-Commerce Application',
-        institute: 'By Myself',
+        institute: 'Self-Studied',
         content: [
             "Developed the backend for the Adora E-Commerce frontend using Spring Boot.",
             "Implemented JWT-based authentication and integrated Spring Security for role-based access control.",
@@ -153,7 +155,7 @@ const timelineData: TimelineData[] = [
             "Handled user registration, login, and protected routes with token validation.",
             "Tested endpoints using Postman and ensured secure data flow across the system."
         ],
-        type: "Learned Skills and Tech Stack :",
+        type: "Skills:",
         skills: ["Spring Boot", "Spring Security", "JWT", "REST", "Postman"],
         date: "Jul 2024 – Jan 2025"
     },
@@ -167,7 +169,7 @@ const timelineData: TimelineData[] = [
             "Learned how React DOM works and how it efficiently updates and renders UI components.",
             "Worked with MongoDB for data persistence and implemented full CRUD operations across the stack."
         ],
-        type: "Learned Skills :",
+        type: "Skills:",
         skills: ["Spring Boot", "Spring Security", "JWT", "React", "MongoDB", "REST", "Postman"],
         date: "September 16, 2025"
     },
@@ -182,18 +184,24 @@ const timelineData: TimelineData[] = [
             "Completed free digital training and passed the official certification exam (1Z0-1122-25).",
             "Gained foundational knowledge in deploying and managing AI solutions on OCI."
         ],
-        type: "Learned Skills :",
-        skills: [
-            "Python", "OCI", "GenAI", "Vector DB", "Select AI",
-            "Machine Learning", "Deep Learning", "Model Evaluation",
-            "Prompt Engineering", "AI Use Cases", "OCI Data Science"
-        ],
-        date: "20 October 2025"
+        type: "Skills:",
+        skills: ["Python", "OCI", "GenAI", "Vector DB", "Select AI", "Machine Learning", "Deep Learning", "Model Evaluation", "Prompt Engineering", "AI Use Cases", "OCI Data Science"],
+        date: "20 October 2025",
+        images: [
+            {
+                img: oci_foundations_associate,
+                link: ""
+            },
+            {
+                img: oci_foundations_associate_badge,
+                link: "https://catalog-education.oracle.com/ords/certview/sharebadge?id=BD94091D9DB1685A40622FC04A0A96A4B5072121BCA35856899DBA3794320269"
+            }
+        ]
     },
     {
         side: 'left',
         title: 'OCI 2025 Certified Generative AI Professional',
-        institute: 'By Myself',
+        institute: 'Self-Studied',
         content: [
             "Earned the OCI Generative AI Professional certification by completing Oracle's Race to Certification 2025 program.",
             "Studied advanced Generative AI concepts including Large Language Models (LLMs), Retrieval-Augmented Generation (RAG), and AI agents.",
@@ -201,7 +209,7 @@ const timelineData: TimelineData[] = [
             "Explored prompt engineering, model fine-tuning, and responsible AI practices.",
             "Passed the official certification exam (1Z0-1127-25) demonstrating proficiency in applying GenAI solutions on OCI."
         ],
-        type: "Learned Skills",
+        type: "Skills:",
         skills: [
             "Generative AI", "LLMs", "RAG", "Prompt Engineering", "OCI AI Services",
             "Model Fine-Tuning", "AI Agents", "Responsible AI", "Python", "Oracle Cloud"
@@ -219,12 +227,12 @@ const timelineData: TimelineData[] = [
             "Supported contributors through challenges and ensured quality across both frontend and backend development.",
             "Built the application using React with TypeScript and Tailwind CSS for the frontend, and Spring Boot for the backend."
         ],
-        type: "Learned Skills and Tech Stack",
+        type: "Skills:",
         skills: ["React", "TypeScript", "Tailwind CSS", "Spring Boot", "Project Management", "Mentorship"],
         date: "28 Oct 2025 – Present"
     }
+].reverse();
 
-];
 
 export default function () {
     return (
@@ -234,7 +242,7 @@ export default function () {
 
 
             <div className="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50 mt-20">
-                {timelineData.reverse().map((item, index) => (
+                {timelineData.map((item, index) => (
                     <div
                         key={index}
                         className={`flex ${item.side === 'left' ? 'flex-row-reverse md:contents' : 'md:contents'}`}
@@ -263,7 +271,7 @@ export default function () {
 
                             {item.skills.length !== 0 && (
                                 <div>
-                                    <h1 className="mt-2 mb-1 text-gray-300 font-semibold">{item.type}</h1>
+                                    <h1 className="mt-3 mb-1 text-gray-300 font-semibold">{item.type}</h1>
                                     <div className="flex flex-wrap gap-2 items-center">
 
                                         {item.skills.map((skill, index) => (
@@ -275,6 +283,24 @@ export default function () {
                                             </div>
                                         ))}
                                     </div>
+                                </div>
+                            )}
+
+                            {item.images?.length !== 0 && (
+                                <div className="flex flex-wrap p-5 gap-3">
+                                        {item.images?.map((img, i) => (
+                                            <div key={i} className="w-32 h-24 rounded overflow-hidden shadow-sm relative group cursor-pointer">
+                                                <div
+                                                    className="w-full h-full bg-center bg-cover"
+                                                    style={{ backgroundImage: `url(${img.img})` }}
+                                                    role="img"
+                                                    aria-label={img.link ? 'certificate' : 'image'}
+                                                />
+                                                <div className="absolute inset-0 flex items-center justify-center bg-gray-300/80 opacity-0 group-hover:opacity-100 transition-opacity">
+                                                    <span className="text-sm font-semibold text-gray-800"><GiTiredEye size={40} /></span>
+                                                </div>
+                                            </div>
+                                        ))}
                                 </div>
                             )}
 
